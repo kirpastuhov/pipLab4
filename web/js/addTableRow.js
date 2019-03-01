cb2201ef441a149cc3dd303a3fa477d344b0063c
Vladislav Saraev <work.saraev@gmail.com>
2019-03-01 21:04:46 +0300

    fix date format

--- web/js/addTableRow.js
+++ web/js/addTableRow.js
@@ -25,7 +25,7 @@ function addTableRow() {
                             json[i].isPointInArea +
                         "</td>" +
                         "<td>" +
-                            moment.unix(Math.round(json[i].currentTime/1000)).format("MMM Do YY") +
+                            moment.unix(Math.round(json[i].currentTime/1000)).format("MMMM Do YYYY, h:mm:ss a") +
                         "</td>" +
                         "<td>" +
                             json[i].executionTime +
