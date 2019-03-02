da2b1cee8a2dc4112e5f418e998c7e765a0805c9
Kirill <kir.pastuhov2016@ya.ru>
2019-03-02 19:31:53 +0300

    Fixed AreaChecker

--- src/controller/AreaCheckerController.java
+++ src/controller/AreaCheckerController.java
@@ -18,32 +18,47 @@ public class AreaCheckerController {
                 isPointInArea = false;
             }
             else {
-                isPointInArea = X * X + Y * Y <= (R * R);
+                isPointInArea = X * X + Y * Y <= (R * R); // circle
+            }
+        } else {
+            if (Y > 0) {
+                isPointInArea = X >= -R && Y <= R; // square
+            } else {
+                isPointInArea = Y <= X + R / 2; //triangle
+
             }
         }
-        if((X < 0 && X < R) || X > 0){
-            isPointInArea = false;
-        }
-        if((Y > 0 && Y > R) || (Y < 0 && Y < R)){
-            isPointInArea = false;
-        }
-        if (Y > 0) {
-            isPointInArea = Y <= X + R;
-        } else {
-            isPointInArea =  X >= -R / 2 && Y >= -R;
-        }
-        double a = (-R/2 - X) * (0 - 0) - (0 + R/2) * (0 - Y);
-        double b = (0 - X) * (-R - 0) - (0 + R) * (0 - Y);
-        double c = (-R - X) * (0 + R) - (-R/2 - 0) * (-R - Y);
-
-        if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
-        {
-            isPointInArea = true;
-        }
-        else
-        {
-            isPointInArea = false;
-        }
+//        if (X > 0) {
+//            if (Y > 0) {
+//                isPointInArea = false;
+//            }
+//            else {
+//                isPointInArea = X * X + Y * Y <= (R * R);
+//            }
+//        }
+//        if((X < 0 && X < R) || X > 0){
+//            isPointInArea = false;
+//        }
+//        if((Y > 0 && Y > R) || (Y < 0 && Y < R)){
+//            isPointInArea = false;
+//        }
+//        if (Y > 0) {
+//            isPointInArea = Y <= X + R;
+//        } else {
+//            isPointInArea =  X >= -R / 2 && Y >= -R;
+//        }
+//        double a = (-R/2 - X) * (0 - 0) - (0 + R/2) * (0 - Y);
+//        double b = (0 - X) * (-R - 0) - (0 + R) * (0 - Y);
+//        double c = (-R - X) * (0 + R) - (-R/2 - 0) * (-R - Y);
+//
+//        if ((a >= 0 && b >= 0 && c >= 0) || (a <= 0 && b <= 0 && c <= 0))
+//        {
+//            isPointInArea = true;
+//        }
+//        else
+//        {
+//            isPointInArea = false;
+//        }
         int user_id = Integer.parseInt(session.getAttribute("Id").toString());
         return new P4_HitData(X, Y, R, (System.nanoTime() - currentTime) / 1000000000d, System.currentTimeMillis(),user_id, isPointInArea);
     }
